25d7b6d28dbcdd4f825173b2abf04aa465f73ec5
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2021-12-12 19:16:22 +0100

    added navbar and navigation functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
-//import AccordionWidget from './Accordion/AccordionWidget';
-//import SearchWidget from './Search/SearchWidget';
-//import DropdownWidget from './Dropdown/DropdownWidget';
+import AccordionWidget from './Accordion/AccordionWidget';
+import SearchWidget from './Search/SearchWidget';
+import DropdownWidget from './Dropdown/DropdownWidget';
 import TranslateWidget from './Translate/TranslateWidget';
+import Route from './Route';
+import Navbar from './Navbar';
 
 const App = () => {
     return (
         <div>
-            <TranslateWidget />
+            <Navbar />
+            <Route path="/">
+                <AccordionWidget />
+            </Route>
+            <Route path="/search">
+                <SearchWidget />
+            </Route>
+            <Route path="/dropdown">
+                <DropdownWidget />
+            </Route>
+            <Route path="/translate">
+                <TranslateWidget />
+            </Route>
         </div>
     );
 };
